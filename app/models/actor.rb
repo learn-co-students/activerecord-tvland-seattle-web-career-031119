b1ece053cba39b42bacea44fc50323fa9966a1b7
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-24 16:38:42 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+
 end
